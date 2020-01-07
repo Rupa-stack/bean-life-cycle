75a26911f0f3ea0173bcbc6fb3e24315626e5fa3
rOOPS <59355508+Rupa-stack@users.noreply.github.com>
2020-01-07 10:11:02 +0530

    Add files via upload

---
+++ spring-bean-life-cycle/src/com/cts/spring/Test.java
@@ -0,0 +1,11 @@
+package com.cts.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        context.close();
+	}
+}
